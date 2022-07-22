5d98b096d86d7ef3166b40479167c4d21c3089c0
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-07-21 22:35:51 -0300

    Add e remove elementos do carrinho

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const products = document.querySelector(".products");
+const shoppingCartContent = document.querySelector(".shopping-cart-content");
+const shoppingCartData = [];
 function createCard(product) {
     const tagLi = document.createElement("li");
     const productImg = document.createElement("img");
@@ -17,6 +19,11 @@ function createCard(product) {
     productDescription.classList.add("product-description");
     productPrice.classList.add("product-price");
     productButton.classList.add("product-button");
+    productButton.addEventListener("click", () => {
+        shoppingCartData.push(product);
+        shoppingCartContent.innerHTML = "";
+        showCart(shoppingCartData);
+    });
     productTitle.innerText = product.nameItem;
     productDescription.innerText = product.description;
     productPrice.innerText = `R$ ${product.value}`;
@@ -34,6 +41,42 @@ function createCard(product) {
 function showProducts(database) {
     database.forEach((product) => {
         products.appendChild(createCard(product));
+    }
+    );
+}
+function createCartItem(product) {
+    const tagLi = document.createElement("li");
+    const shoppingCartItemImg = document.createElement("img");
+    const shoppingCartItemInfo = document.createElement("div");
+    const shoppingCartItemTitle = document.createElement("h3");
+    const shoppingCartItemPrice = document.createElement("p");
+    const shoppingCartItemButton = document.createElement("button");
+    tagLi.classList.add("shopping-cart-item");
+    shoppingCartItemImg.src = product.img;
+    shoppingCartItemImg.classList.add("shopping-cart-item-img");
+    shoppingCartItemInfo.classList.add("shopping-cart-item-info");
+    shoppingCartItemTitle.classList.add("shopping-cart-item-title");
+    shoppingCartItemPrice.classList.add("shopping-cart-item-price");
+    shoppingCartItemButton.classList.add("shopping-cart-item-button");
+    shoppingCartItemButton.addEventListener("click", () => {
+        shoppingCartData.splice(shoppingCartData.indexOf(product), 1);
+        shoppingCartContent.innerHTML = "";
+        showCart(shoppingCartData);
+    }
+    );
+    shoppingCartItemTitle.innerText = product.nameItem;
+    shoppingCartItemPrice.innerText = `R$ ${product.value}`;
+    shoppingCartItemButton.innerText = "Remover";
+    shoppingCartItemInfo.appendChild(shoppingCartItemTitle);
+    shoppingCartItemInfo.appendChild(shoppingCartItemPrice);
+    shoppingCartItemInfo.appendChild(shoppingCartItemButton);
+    tagLi.appendChild(shoppingCartItemImg);
+    tagLi.appendChild(shoppingCartItemInfo);
+    return tagLi;
+}
+function showCart(database) {
+    database.forEach((product) => {
+        shoppingCartContent.appendChild(createCartItem(product));
     }
     );
 }
