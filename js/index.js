b44752730a3f59fe4885a05edf30bc0b7c6fe30c
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-07-21 22:10:09 -0300

    removendo lis e adicionando-os via js, manipulando o dom

--- js/index.js
+++ js/index.js
@@ -1,9 +1,40 @@
+const products = document.querySelector(".products");
 function createCard(product) {
     const tagLi = document.createElement("li");
-    const tagImg = document.createElement("img");
-    const divProductInfo = document.createElement("div");
-    const divProductCategory = document.createElement("div");
-    const tagH2 = document.createElement("div");
+    const productImg = document.createElement("img");
+    const productInfo = document.createElement("div");
+    const productCategory = document.createElement("div");
+    const productTitle = document.createElement("h2");
+    const productDescription = document.createElement("p")
+    const productPrice = document.createElement("p");
+    const productButton = document.createElement("button");
+    tagLi.classList.add("product");
+    productImg.src = product.img;
+    productImg.classList.add("product-img");
+    productInfo.classList.add("product-info");
+    productCategory.classList.add("product-category");
+    productTitle.classList.add("product-title");
+    productDescription.classList.add("product-description");
+    productPrice.classList.add("product-price");
+    productButton.classList.add("product-button");
+    productTitle.innerText = product.nameItem;
+    productDescription.innerText = product.description;
+    productPrice.innerText = `R$ ${product.value}`;
+    productButton.innerText = product.addCart;
+    productCategory.innerText = product.tag[0];
+    productInfo.appendChild(productCategory);
+    productInfo.appendChild(productTitle);
+    productInfo.appendChild(productDescription);
+    productInfo.appendChild(productPrice);
+    productInfo.appendChild(productButton);
+    tagLi.appendChild(productImg);
+    tagLi.appendChild(productInfo);
+    return tagLi;
 }
-
-console.log("olá");
+function showProducts(database) {
+    database.forEach((product) => {
+        products.appendChild(createCard(product));
+    }
+    );
+}
+showProducts(data);
