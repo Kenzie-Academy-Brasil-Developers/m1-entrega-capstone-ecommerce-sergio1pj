70ffeed071d8f7bb55bb3833724cee7fd5e76c13
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-07-21 21:54:26 -0300

    Tornando o site dinâmico

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function createCard(product) {
+    const tagLi = document.createElement("li");
+    const tagImg = document.createElement("img");
+    const divProductInfo = document.createElement("div");
+    const divProductCategory = document.createElement("div");
+    const tagH2 = document.createElement("div");
+}
+
+console.log("olá");
